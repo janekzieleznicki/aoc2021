f114edfc18eefa18c1eec1eb06823c8b09c7a6d9
Janek Zieleźnicki <janek.zieleznicki@gmail.com>
2021-12-05 21:16:00 +0100

    day5 solved

---
+++ day5/src/board.rs
@@ -0,0 +1,63 @@
+use crate::line::Line;
+
+#[derive(Debug, Clone)]
+pub struct Board {
+    fields: ndarray::Array2<u16>,
+}
+
+impl Board {
+    pub(crate) fn marked_at_least(&self, p0: u16) -> usize {
+        self.fields.iter()
+            .filter(|&&marks|marks>=p0)
+            .count()
+    }
+}
+
+
+impl Board {
+    pub fn mark(&mut self, line: Line) {
+        assert!(line.is_horizontal_or_vertical(), "Only horizontal and vertical are supported for now");
+        line.point_vec().into_iter().for_each(|point|self.fields[[point.x as usize,point.y as usize]] += 1);
+    }
+    pub fn mark_part2(&mut self, line: Line) {
+        assert!(line.is_horizontal_vertical_or_diagonal(), "Only horizontal and vertical are supported for now");
+        line.point_vec().into_iter().for_each(|point|self.fields[[point.x as usize,point.y as usize]] += 1);
+    }
+    pub fn with_dim(dim: usize) -> Board {
+        Board {
+            fields: ndarray::Array2::from_elem((dim, dim), 0),
+        }
+    }
+}
+
+#[cfg(test)]
+mod board_tests {
+    use crate::board::Board;
+
+    #[test]
+    fn mark_test() {
+        let mut board = Board::with_dim(10);
+        board.mark("1,1 -> 1,3".parse().unwrap());
+        board.mark("9,7 -> 7,7".parse().unwrap());
+        println!("{:?}",board);
+        assert_eq!(board.fields[[1,1]],1);
+        assert_eq!(board.fields[[1,2]],1);
+        assert_eq!(board.fields[[1,3]],1);
+        assert_eq!(board.fields[[9,7]],1);
+        assert_eq!(board.fields[[8,7]],1);
+        assert_eq!(board.fields[[7,7]],1);
+    }
+    #[test]
+    fn part2_mark_test() {
+        let mut board = Board::with_dim(10);
+        board.mark_part2("1,1 -> 3,3".parse().unwrap());
+        board.mark_part2("9,7 -> 7,9".parse().unwrap());
+        println!("{:?}",board);
+        assert_eq!(board.fields[[1,1]],1);
+        assert_eq!(board.fields[[2,2]],1);
+        assert_eq!(board.fields[[3,3]],1);
+        assert_eq!(board.fields[[9,7]],1);
+        assert_eq!(board.fields[[8,8]],1);
+        assert_eq!(board.fields[[7,9]],1);
+    }
+}
